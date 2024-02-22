67f3e74c57c866319b47fb6df1aa34fc6fe0b8a4
nesty11 <arielvale8@hotmail.com>
2024-02-22 17:48:58 -0500

    fixed components shifting when opening keyboard
    changed background cover and background color selection

--- components/Start.js
+++ components/Start.js
@@ -12,12 +12,12 @@ import {
 const StartScreen = ({ navigation }) => {
   const [name, setName] = useState("");
   const [background, setBackground] = useState("");
-  const colors = ["#1DB954", "#1877F2", "#C0C0C0", "#FF9900"];
+  const colors = ["#090C08", "#474056", "#8A95A5", "#B9C6AE"];
 
   return (
     <View style={styles.container}>
       <ImageBackground
-        source={require("../img/cover.png")}
+        source={require("../img/bgcover.png")}
         style={styles.bgImage}
         resizeMode="cover"
       >
@@ -30,35 +30,37 @@ const StartScreen = ({ navigation }) => {
             onChangeText={setName}
             placeholder="Type your username here..."
           />
-          <Text style={styles.chooseBgColor}>Choose A Background Color</Text>
-          {/* Choose a background color for your chat */}
-          <View style={styles.colorButtonsBox}>
-            {colors.map((color, index) => (
-              <TouchableOpacity
-                key={index}
-                style={[
-                  styles.colorButton,
-                  { backgroundColor: color },
-                  background === color && styles.selected,
-                ]}
-                onPress={() => {
-                  setBackground(color);
-                }}
-              />
-            ))}
+          <View style={styles.bottomContent}>
+            <Text style={styles.chooseBgColor}>Choose A Background Color</Text>
+            {/* Choose a background color for your chat */}
+            <View style={styles.colorButtonsBox}>
+              {colors.map((color, index) => (
+                <TouchableOpacity
+                  key={index}
+                  style={[
+                    styles.colorButton,
+                    { backgroundColor: color },
+                    background === color && styles.selected,
+                  ]}
+                  onPress={() => {
+                    setBackground(color);
+                  }}
+                />
+              ))}
+            </View>
+            {/* Start Chat */}
+            <TouchableOpacity
+              style={[styles.button, styles.touchableOpacityButton]}
+              onPress={() => {
+                navigation.navigate("Chat", {
+                  name: name,
+                  background: background,
+                });
+              }}
+            >
+              <Text style={styles.buttonText}>Start Chat</Text>
+            </TouchableOpacity>
           </View>
-          {/* Start Chat */}
-          <TouchableOpacity
-            style={[styles.button, styles.touchableOpacityButton]}
-            onPress={() => {
-              navigation.navigate("Chat", {
-                name: name,
-                background: background,
-              });
-            }}
-          >
-            <Text style={styles.buttonText}>Start Chat</Text>
-          </TouchableOpacity>
         </View>
       </ImageBackground>
     </View>
@@ -72,51 +74,52 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   textInput: {
-    width: "88%",
+    width: "100%",
     padding: 15,
     borderWidth: 1,
     marginTop: 15,
     marginBottom: 15,
     fontSize: 16,
     fontWeight: "300",
-    opacity: 50,
-    justifyContent: "center",
-    alignItems: "center",
     borderRadius: 20,
   },
   bgImage: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    width: 400,
-    height: 1000,
+    width: "100%",
+    height: "100%",
   },
   appTitle: {
-    fontSize: 50,
+    fontSize: 45,
     fontWeight: "600",
-    color: "#F79194",
+    color: "#FFFFFF",
     margin: 20,
-    top: -150,
+    position: "absolute",
+    top: 50,
   },
   box: {
     backgroundColor: "#ffffff",
     padding: 30,
-    width: "88%",
-    height: "35%",
+    width: "80%",
     alignItems: "center",
     justifyContent: "center",
     borderRadius: 20,
   },
-  chooseBackgroundColor: {
-    flex: 1,
+  bottomContent: {
+    width: "100%",
+    alignItems: "center",
+  },
+  chooseBgColor: {
     fontSize: 16,
     fontWeight: "300",
     color: "#757083",
+    marginBottom: 10,
   },
   colorButtonsBox: {
     flexDirection: "row",
-    justifyContent: "space-between",
-    margin: 20,
+    justifyContent: "center",
+    marginBottom: 10,
   },
   colorButton: {
     width: 50,
@@ -132,19 +135,14 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "#757083",
     padding: 10,
+    width: "50%",
+    borderRadius: 5,
+    marginTop: 10,
   },
   buttonText: {
     fontSize: 16,
     fontWeight: "600",
     color: "#ffffff",
-  },
-  touchableOpacityButton: {
-    alignItems: "center",
-    backgroundColor: "#757083",
-    padding: 10,
-    width: "40%",
-    borderRadius: 5,
-    marginVertical: 10,
   },
 });
 
