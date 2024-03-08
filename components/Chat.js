555d64ffa2964291f83cec8507f1932e2863776c
nesty11 <arielvale8@hotmail.com>
2024-03-07 20:19:27 -0500

    added offline capabilities for app
    using AsyncStorage and cache for better app functionality

--- components/Chat.js
+++ components/Chat.js
@@ -1,4 +1,4 @@
-import { Bubble, GiftedChat } from "react-native-gifted-chat";
+import { Bubble, GiftedChat, InputToolbar } from "react-native-gifted-chat";
 import { useEffect, useState } from "react";
 import { StyleSheet, View, KeyboardAvoidingView, Platform } from "react-native";
 import {
@@ -8,32 +8,62 @@ import {
   query,
   orderBy,
 } from "firebase/firestore";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const Chat = ({ navigation, route, db }) => {
+const Chat = ({ navigation, route, db, isConnected }) => {
   const { name, background, id } = route.params;
   const [messages, setMessages] = useState([]);
   const onSend = (newMessages) => {
     addDoc(collection(db, "messages"), newMessages[0]);
   };
 
-  useEffect(() => {
-    const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
-    const unsubMessages = onSnapshot(q, (DocumentSnapshot) => {
-      let newMessages = [];
-      DocumentSnapshot.forEach((doc) => {
-        newMessages.push({
-          id: doc.id,
-          ...doc.data(),
-          createdAt: new Date(doc.data().createdAt.toMillis()),
-        });
-      });
-      setMessages(newMessages);
-    });
+  let unsubMessages;
 
+  useEffect(() => {
+    if (isConnected === true) {
+      //unregister current onSnapshot() listener to avoid registering multiple listeners when useEffect code is re-executed
+      if (unsubMessages) unsubMessages();
+      unsubMessages = null;
+
+      const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
+      unsubMessages = onSnapshot(q, (DocumentSnapshot) => {
+        let newMessages = [];
+        DocumentSnapshot.forEach((doc) => {
+          newMessages.push({
+            id: doc.id,
+            ...doc.data(),
+            createdAt: new Date(doc.data().createdAt.toMillis()),
+          });
+        });
+        cachedMessages(newMessages);
+        setMessages(newMessages);
+      });
+    } else loadCachedMessages();
+
+    //Clean up code
     return () => {
       if (unsubMessages) unsubMessages();
     };
-  }, []);
+  }, [isConnected]);
+
+  const loadCachedMessages = async () => {
+    const cachedMessages = (await AsyncStorage.getItem("chat_messages")) || [];
+    setMessages(JSON.parse(cachedMessages));
+  };
+
+  const cachedMessages = async (listsToCache) => {
+    try {
+      await AsyncStorage.setItem("chat_messages", JSON.stringify(listsToCache));
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  // Prevent rendering of InputToolbar when offline
+  const renderInputToolbar = (props) => {
+    if (isConnected) return <InputToolbar {...props} />;
+    else return null;
+  };
 
   useEffect(() => {
     navigation.setOptions({ title: name });
@@ -60,6 +90,7 @@ const Chat = ({ navigation, route, db }) => {
       <GiftedChat
         messages={messages}
         renderBubble={renderBubble}
+        renderInputToolbar={renderInputToolbar}
         onSend={(messages) => onSend(messages)}
         user={{ _id: id }}
       />
